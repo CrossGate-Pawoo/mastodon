c6ef4dce0b5d9b7369785eb764e4f031f53df367
abcang <abcang1015@gmail.com>
2020-02-20 15:52:01 +0900

    Fix name space

--- app/helpers/pawoo/pixiv_auth_helper.rb
+++ app/helpers/pawoo/pixiv_auth_helper.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-module Pawoo
-  module PixivAuthHelper
-    def pawoo_pixiv_auth_url
-      login_params = { view_type: 'page', source: 'pawoo', return_to: user_pixiv_omniauth_authorize_url }
-      "#{Rails.configuration.x.accounts_pixiv_url}/login?#{login_params.to_query}"
-    end
+module Pawoo::PixivAuthHelper
+  def pawoo_pixiv_auth_url
+    login_params = { view_type: 'page', source: 'pawoo', return_to: user_pixiv_omniauth_authorize_url }
+    "#{Rails.configuration.x.accounts_pixiv_url}/login?#{login_params.to_query}"
   end
 end
