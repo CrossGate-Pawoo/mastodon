fd47536c82c0c763bffc69b0a773f3a2607a58f8
abcang <abcang1015@gmail.com>
2020-02-20 15:34:05 +0900

    Cleanup pixiv login

--- app/controllers/pawoo/follow_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Pawoo::FollowController < ActionController::Base
-  def queue
-    session['pawoo.follow'] = params[:follow] if any_authenticity_token_valid?
-    head 200
-  end
-end
