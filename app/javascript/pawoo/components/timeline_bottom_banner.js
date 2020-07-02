7779954e94ac6e1aeda77754aac2f49131a533c7
Landon <landonchen@sheenagames.com>
2020-07-02 16:39:59 +0800

    Send a GA event when click the banner

--- app/javascript/pawoo/components/timeline_bottom_banner.js
+++ app/javascript/pawoo/components/timeline_bottom_banner.js
@@ -4,9 +4,13 @@ import banner from '../../pawoo/images/about/banner.png';
 export default function TimelineBottomBanner() {
   return (
     <div className='pawoo-kyoa-home'>
-      <a href='http://www.waffle1999.com/game/edensgrenze/'>
+      <a href='http://www.waffle1999.com/game/edensgrenze/' onClick={handleTBBClick}>
         <img alt='banner' src={banner} />
       </a>
     </div>
   );
-}
+};
+
+export function handleTBBClick() {
+  ga('send', 'event', 'TimelineBottomBanner', 'click', 'http://www.waffle1999.com/game/edensgrenze/');
+};
